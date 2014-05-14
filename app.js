f4595ed68f59d264551ce82b4ef146a625cfbee3
Daniel Vergara <daver182@gmail.com>
2014-05-14 17:53:50 -0400

    Se añade Favicon, se cambian algunos valores

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -10,7 +8,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -20,10 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+	res.render('index');
+});
 app.post('/play', function(req, res){
 	var positions = req.body.positions;
 	if(positions){
@@ -85,5 +85,5 @@ app.post('/play', function(req, res){
 
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
